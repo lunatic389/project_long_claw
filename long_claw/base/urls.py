2853f8adfc3ca9663e9cf2bbc688ce3df5fa892f
lunatic389 <53988530+lunatic389@users.noreply.github.com>
2020-06-23 05:28:02 +0530

    first commit

---
+++ long_claw/base/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name='base'
+
+
+urlpatterns = [
+    path('hompage/',views.index,name='home'),
+    path('contact/',views.contact,name='contact'),
+    path('register/',views.register,name='register'),
+    path('user_login/',views.user_login,name='user_login'),
+    path('search/',views.search,name='search')
+
+]
