d15ad036f6f005a4c2aeef687b5822fd04f8f4fe
rajasnathak <61021680+rajasnathak@users.noreply.github.com>
2020-06-23 16:31:03 +0530

    Add model in base app

--- long_claw/base/models.py
+++ long_claw/base/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.forms import ModelForm
 
-# Create your models here.
+BLOOD_TYPE = [
+    ('A+'), ('A-'),
+    ('B+'), ('B-.'),
+    ('AB+'), ('AB-'),
+    ('O+'), ('O-'),
+]
+
+class Info(models.Model):
+    name = models.CharField(max_length=100)
+    blood_type = models.CharField(max_length=3, choices=BLOOD_TYPE)
+    resident_state = models.CharField(max_length=2)
+    birth_date = models.DateField(blank=True, null=True)
+    phone_number = models.CharField(max_length=10)
