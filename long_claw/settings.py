951d0815d16a0d974e83a8abd985d017f867ff50
lunatic389 <53988530+lunatic389@users.noreply.github.com>
2021-07-17 12:17:33 +0530

    adding requirement.txt

--- long_claw/settings.py
+++ long_claw/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,3 +128,5 @@ MEDIA_ROOT=MEDIA_DIR
 MEDIA_URL = '/media/'
 
 LOGIN_URL = 'base/user_login'
+
+django_heroku.settings(locals())
